16c692d96fdd1c1d92b89b89844931e61c74ea8f
Axel Ingi Kristinsson <axelingi94@gmail.com>
2016-10-26 16:12:49 +0000

    utfaerdi displayBoard fallid til ad taka a moti board fra Game

--- src/main/java/is/ru/tictactoe/TicTacToe.java
+++ src/main/java/is/ru/tictactoe/TicTacToe.java
@@ -6,33 +6,33 @@ public class TicTacToe {
 
         public void play() {
 
+                Game game = new Game();
+                displayBoard(game.getBoard());
+
         }
 
         private char getInput() {
                 return '0';
         }
 
-        public void displayBoard() {
-                char[][] board = new char[3][3];
-                for(int i = 0; i < 3; i++){
-                        for(int j = 0; j < 3; j++){
-                                if(i % 2 == 0 && j % 2 == 1)
-                                        board[i][j] = 'X';
-                                else
-                                        board[i][j] = 'O';
+        public void displayBoard(char[][] board) {
 
-                                StdOut.printf(board[i][j] + " ");
+                for(int i = 0;i <3;i++)
+                {
+                        StdOut.println(" _____________");
+                        for(int j = 0;j < 3;j++)
+                        {
+                                StdOut.print(" | " + board[i][j]);
                         }
-                        StdOut.printf("\n");
+                        StdOut.print(" |\n");
                 }
-
+                StdOut.println(" _____________");
         }
 
         public static void main(String[] args) {
 
-                TicTacToe burd = new TicTacToe();
-                burd.displayBoard();
-                StdOut.println("TicTacToooooe");
+                TicTacToe game = new TicTacToe();
+                game.play();
         }
 }
 
