a42253134b2cd7a5cf6c7b153a02975c5cfe38fc
hjalmarh15 <hjalmarh15@ru.is>
2016-10-26 12:23:08 +0000

    Contructor og get föll í Game

--- src/main/java/is/ru/tictactoe/Game.java
+++ src/main/java/is/ru/tictactoe/Game.java
@@ -5,20 +5,43 @@ import edu.princeton.cs.algs4.*;
 
 
 public class Game {
+		/*
+		board looks like this:
+			1 2 3
+			4 5 6
+			7 8 9
+		*/
 		private char[][] board;
-		private char player;
-		private boolean draw;
+		private char player; //represents the player whose turn it is, X or O
+		private boolean draw; //represents if there is a draw in the game
 
-		public void turn(char input) {
+		public Game() {
+			board = new char[3][3];
+			player = 'X';
+			draw = false;
+			initializeBoard(board);
+		}
+
+		private void initializeBoard(char[][] board) {
+			char counter = 49;	//counter starts at ascii value for 1
+			for(int i=0; i < 3; i++) {
+				for(int j = 0; j < 3; j++) {
+					board[i][j] = counter;
+					counter++; //counter is incremented
+				}
+			}
+		}
+
+		private void turn(char input) {
 
 		}
 
 		public char[][] getBoard() {
-			return new char[3][3];
+			return board;
 		}
 
 		public char getPlayer() {
-			return '0';
+			return player;
 		}
 
 		public boolean checkIfInputIsValid(char input) {
@@ -27,5 +50,15 @@ public class Game {
 
 		private boolean gameOver(char[][] board) {
 			return false;
+		}
+
+		public static void main(String[] args) {
+			Game theGame = new Game();
+			char[][] board = theGame.getBoard();
+			for(int i = 0; i < 3; i++) {
+				for(int j = 0; j < 3; j++) {
+					StdOut.println(board[i][j]);
+				}
+			}
 		}
 	}
