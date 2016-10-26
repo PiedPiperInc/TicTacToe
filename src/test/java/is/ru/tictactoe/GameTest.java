70bf409d640f7ca7327d1aa3fb0e25f7396430c2
hjalmarh15 <hjalmarh15@ru.is>
2016-10-26 12:35:44 +0000

    baetti vid gametest skra

---
+++ src/test/java/is/ru/tictactoe/GameTest.java
@@ -0,0 +1,19 @@
+package is.ru.tictactoe;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class GameTest {
+	@Test
+	public void testInitializeBoard() {
+		Game theGame = new Game();
+		char[][] theBoard = theGame.getBoard();
+		char counter = 49; //counter is char value of 1
+		for(int i=0; i < 3; i++) {
+			for(int j = 0; j < 3; j++) {
+				assertEquals(counter, theBoard[i][j]);
+				counter++; 
+			}
+		}
+	}
+}
