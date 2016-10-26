72a1f2991983fc6f77aeb4fdcf808abb05f12033
Axel Ingi Kristinsson <axelingi94@gmail.com>
2016-10-26 16:20:34 +0000

    bætti við commentum

--- src/main/java/is/ru/tictactoe/TicTacToe.java
+++ src/main/java/is/ru/tictactoe/TicTacToe.java
@@ -5,8 +5,10 @@ import edu.princeton.cs.algs4.*;
 public class TicTacToe {
 
         public void play() {
-
+                //initialize a new Game. This fills the board with numbers 1-9
                 Game game = new Game();
+
+                //display the board with the board from the game class
                 displayBoard(game.getBoard());
 
         }
@@ -16,7 +18,15 @@ public class TicTacToe {
         }
 
         public void displayBoard(char[][] board) {
-
+                /*board should look like this on the console
+                         _____________
+                         | 1 | 2 | 3 |
+                         _____________
+                         | 4 | 5 | 6 |
+                         _____________
+                         | 7 | 8 | 9 |
+                         _____________
+                */
                 for(int i = 0;i <3;i++)
                 {
                         StdOut.println(" _____________");
@@ -30,7 +40,7 @@ public class TicTacToe {
         }
 
         public static void main(String[] args) {
-
+                //main function should only need to create a Game instance and run play()
                 TicTacToe game = new TicTacToe();
                 game.play();
         }
