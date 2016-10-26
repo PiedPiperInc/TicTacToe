9f8b6571d2833ff1bb62ec031ea7528cfb580354
hjalmarh15 <hjelmut@gmail.com>
2016-10-26 16:23:27 +0000

    Revert "Display board"

--- src/main/java/is/ru/tictactoe/Game.java
+++ src/main/java/is/ru/tictactoe/Game.java
@@ -5,55 +5,27 @@ import edu.princeton.cs.algs4.*;
 
 
 public class Game {
-		/*
-		board looks like this:
-			1 2 3
-			4 5 6
-			7 8 9
-		*/
 		private char[][] board;
-		private char player; //represents the player whose turn it is, X or O
-		private boolean draw; //represents if there is a draw in the game
+		private char player;
+		private boolean draw;
 
-		public Game() {
-			board = new char[3][3];
-			player = 'X';
-			draw = false;
-			initializeBoard(board);
-		}
-
-		private void initializeBoard(char[][] board) {
-			char counter = 49;	//counter starts at ascii value for 1
-			for(int i=0; i < 3; i++) {
-				for(int j = 0; j < 3; j++) {
-					board[i][j] = counter;
-					counter++; //counter is incremented
-				}
-			}
-		}
-
-		public  void turn(char input) {
+		public void turn(char input) {
 
 		}
 
 		public char[][] getBoard() {
-			return board;
+			return new char[3][3];
 		}
 
 		public char getPlayer() {
-			return player;
+			return '0';
 		}
 
 		public boolean checkIfInputIsValid(char input) {
-			if(input > 48 && input < 58)
-				return true;
 			return false;
 		}
 
 		private boolean gameOver(char[][] board) {
 			return false;
-		}
-
-		public static void main(String[] args) {
 		}
 	}
--- src/main/java/is/ru/tictactoe/TicTacToe.java
+++ src/main/java/is/ru/tictactoe/TicTacToe.java
@@ -4,45 +4,21 @@ import edu.princeton.cs.algs4.*;
 
 public class TicTacToe {
 
-        public void play() {
-                //initialize a new Game. This fills the board with numbers 1-9
-                Game game = new Game();
+	public void play() {
 
-                //display the board with the board from the game class
-                displayBoard(game.getBoard());
+	}
 
-        }
+	private char getInput() {
+	}
 
-        private char getInput() {
-                return '0';
-        }
+	private void displayBoard(char[][] board) {
 
-        public void displayBoard(char[][] board) {
-                /*board should look like this on the console
-                         _____________
-                         | 1 | 2 | 3 |
-                         _____________
-                         | 4 | 5 | 6 |
-                         _____________
-                         | 7 | 8 | 9 |
-                         _____________
-                */
-                for(int i = 0;i <3;i++)
-                {
-                        StdOut.println(" _____________");
-                        for(int j = 0;j < 3;j++)
-                        {
-                                StdOut.print(" | " + board[i][j]);
-                        }
-                        StdOut.print(" |\n");
-                }
-                StdOut.println(" _____________");
-        }
+	}
 
-        public static void main(String[] args) {
-                //main function should only need to create a Game instance and run play()
-                TicTacToe game = new TicTacToe();
-                game.play();
-        }
+
+	public static void main(String[] args) {
+		Game theGame = new Game();
+		theGame.turn('0');
+		StdOut.println("TicTacToooooe");
+	}
 }
-
