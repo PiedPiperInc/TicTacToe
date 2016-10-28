37bd395d27d1089d6e6fe763d90ea4f8c3c96842
hjalmarh15 <hjalmarh15@ru.is>
2016-10-28 23:43:42 +0000

    baetti vid klosum og doti fyrir webapplication, tok tha ut TicTacToe UI klasann

--- src/main/java/is/ru/tictactoe/Game.java
+++ src/main/java/is/ru/tictactoe/Game.java
@@ -1,31 +1,119 @@
-
 package is.ru.tictactoe;
 
-import edu.princeton.cs.algs4.*;
-
+import net.joningi.icndb.ICNDBClient;
 
 public class Game {
+
+		final ICNDBClient client = new ICNDBClient();
+		/*
+		board looks like this:
+			1 2 3
+			4 5 6
+			7 8 9
+		*/
 		private char[][] board;
-		private char player;
-		private boolean draw;
+		private char player; //represents the player whose turn it is, X or O
+		public boolean draw; //represents if there is a draw in the game
+		public Game() {
+			board = new char[3][3];
+			player = 'X';
+			draw = false;
+			initializeBoard(board);
+		}
 
-		public void turn(char input) {
+		public Game(char[][] board) {
+			this.board = board;
+			player = 'O';
+			draw = false;
+		}
 
+		private void initializeBoard(char[][] board) {
+			char counter = 49;	//counter starts at ascii value for 1
+			for(int i=0; i < 3; i++) {
+				for(int j = 0; j < 3; j++) {
+					board[i][j] = counter;
+					counter++; //counter is incremented
+				}
+			}
+		}
+
+		public char turn(String s) {
+
+			char input = s.charAt(0);
+            if(board[(input - '1')/3][(input - '1')%3] == 'X' || board[(input - '1')/3][(input - '1')%3] == 'O') {
+                return player;
+            }
+
+            else {
+                board[(input - '1')/3][(input - '1')%3] = player;
+                char thePlayer = player;
+                if(player == 'X')
+                    player = 'O';
+                else if(player == 'O')
+                    player = 'X';
+                return thePlayer;
+            }
 		}
 
 		public char[][] getBoard() {
-			return new char[3][3];
+			return board;
 		}
 
 		public char getPlayer() {
-			return '0';
+			return player;
 		}
 
-		public boolean checkIfInputIsValid(char input) {
-			return false;
+		public char checkIfInputIsValid(int input) {
+			if(input >= 1 && input <= 9){
+				input += 48;
+				if(board[(input - '1')/3][(input - '1')%3] != (char) input){ 
+					return '0';
+				} 
+			}
+			else{
+				return '0';
+			}
+			char convert = (char) input;
+				
+			return convert;
 		}
 
-		private boolean gameOver(char[][] board) {
-			return false;
+		public boolean gameOver() {
+
+		//checks for winner horizontally
+		for(int i = 0; i < 3; i++){
+			if(board[i][0] == board[i][1] && board[i][1] == board[i][2]){
+				return true;
+			}	
+		}
+
+		//checks for winner vertically
+		for(int i = 0; i < 3; i++){
+			if(board[0][i] == board[1][i] && board[1][i] == board[2][i]){
+				return true;
+			}	
+		}	
+
+		//checks for winner diagonally
+		if(board[0][0] == board[1][1] && board[1][1] == board[2][2]){
+			return true;
+		}
+
+		//checks for winner diagonally			
+		if(board[0][2] == board[1][1] && board[1][1] == board[2][0]){
+			return true;
+		}
+
+
+		//checks if its a draw
+		for(int i = 0; i < 3; i++){
+			for(int j = 0; j < 3; j++){
+				if(board[i][j] != 'X' && board[i][j] != 'O')
+					return false;
+			}	
+		}
+			draw = true;
+
+		return false;
 		}
 	}
