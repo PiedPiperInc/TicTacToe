3feb359653ed32ec7fc6a5e4880761bb3b49f828
hjalmarh15 <hjalmarh15@ru.is>
2016-10-29 18:06:38 +0000

    setti inn myndir fyrir X og O

--- src/main/resources/public/TicTacToe.js
+++ src/main/resources/public/TicTacToe.js
@@ -1,10 +1,75 @@
 
         var id;
+        var player;
+
         function myFunction(clicked_id) {
             id = clicked_id;
             $('#turnForm').submit();
         };
 
+        function anothaOne() {
+                for(i = 1; i <= 10; i++) {
+                    $('#' + i).html('');
+                    $('#' + i).val('');
+                }
+        }
+
+
+        function gameOver(gameover) {
+            if(gameover == 'true') {
+                alert('Game over! The winner is '  + String(player)  + "!!!!");
+                $('#currentPlayer').html("Player X it's your turn");
+                $('#playAgain').html('<button onclick="anothaOne()" >Play again?</button>');
+            }
+            else if(gameover == 'draw') {
+                alert("Game over! It's a draw! Everybody loses");
+                $('#currentPlayer').html("Player X it's your turn");
+                $('#playAgain').html('<button onclick="anothaOne()" >Play again?</button>');
+            }
+            if(gameover == 'true' || gameover == 'draw') {
+                $.ajax({
+                        type: 'POST',
+                        url: '/getWins',
+                        data: null
+                    }).done(function(wins) {
+                        winners(wins);
+                    });
+            }
+        };
+
+
+
+        function winners(wins) {
+            var result = wins.split(" ");
+            $('#Xwins').html(result[0] + " wins");
+            $('#Owins').html(result[1] + " wins");
+            $('#Draws').html("Draws: " + result[2]);
+        };
+
+
+        function turns() {
+            if($('#' + id).val() != 'hannes' && $('#' + id).val() != 'kristo') {
+                if(player == 'X') { 
+                    $('#' + id).html('<img src="hannes.jpeg"/>');
+                    $('#' + id).val('hannes');
+                    $('#currentPlayer').html("Player O it's your turn");
+                    console.log('x');
+                }
+                else {
+                    $('#' + id).html('<img src="kristo.jpeg"/>');
+                    $('#' + id).val('kristo');
+                    $('#currentPlayer').html("Player X it's your turn");
+                    console.log('o');
+                    }
+            }          
+                 $.ajax({
+                    type: 'POST',
+                    url: '/gameover',
+                    data: null
+                }).done(function(gameover) {
+                    gameOver(gameover);
+                });
+        };
 
         $(document).ready(function() {
 
@@ -15,45 +80,9 @@
                     type: form.attr('method'),
                     url: form.attr('action'),
                     data: 'inputID=' + id
-                }).done(function(player) {
-                    if($('#' + id).text() != 'X' && $('#' + id).text() != 'O') {
-                        $('#' + id).html(String(player));
-                        if(player == 'X')
-                            $('#currentPlayer').html("Player O it's your turn");
-                        else    
-                            $('#currentPlayer').html("Player X it's your turn");
-                    }
-                    $.ajax({
-                    type: 'POST',
-                    url: '/gameover',
-                    data: null
-                }).done(function(gameover) {
-                    if(gameover == 'true') {
-                        for(i = 1; i <= 10; i++) {
-                            $('#' + i).html('');
-                        }
-                        alert('Game over! The winner is '  + String(player)  + "!!!!");
-                    }
-                    else if(gameover == 'draw') {
-                        for(i = 1; i <= 10; i++) {
-                            $('#' + i).html('');
-                        }
-                        alert("Game over! It's a draw! Everybody loses");
-                    }
-
-                    $.ajax({
-                        type: 'POST',
-                        url: '/getWins',
-                        data: null
-                    }).done(function(wins) {
-                        var result = wins.split(" ");
-                        $('#Xwins').html("Player X: " + result[0] + " wins");
-                        $('#Owins').html("Player O: " + result[1] + " wins");
-                        $('#Draws').html("Draws: " + result[2]);
-                    });
-                });   
-                }).fail(function() {
-                    $('#results').html('Error, no joke found...').attr('class', 'alert alert-danger');
+                }).done(function(thePlayer) {
+                    player = thePlayer;
+                    turns();
                 });
                 
                 return false;
